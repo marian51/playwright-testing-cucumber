23df20b7693f00b6ba25759c26613182d637cc70
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-22 17:20:23 +0200

    feat: add class for Login Pag

---
+++ src/page-objects/login-page/LoginPage.ts
@@ -0,0 +1,34 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../BasePage.ts";
+
+export default class LoginPage extends BasePage {
+  private readonly mainContainer: Locator;
+  private readonly loginEmailInput: Locator;
+  private readonly loginPasswordInput: Locator;
+  private readonly submitButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.mainContainer = this.page.getByTestId("login__main-container");
+    this.loginEmailInput = this.mainContainer.getByTestId("login-email-input");
+    this.loginPasswordInput = this.mainContainer.getByTestId("login-password-input");
+    this.submitButton = this.mainContainer.getByRole("button", { name: "Log in" });
+  }
+
+  async goToLoginPage() {
+    await this.page.goto("https://app.clickup.com/");
+    await this.mainContainer.waitFor();
+  }
+
+  async typeIntoLoginInput(text: string) {
+    await this.loginEmailInput.fill(text);
+  }
+
+  async typeIntoPasswordInput(text: string) {
+    await this.loginPasswordInput.fill(text);
+  }
+
+  async clickOnSubmitButton() {
+    await this.submitButton.click();
+  }
+}
